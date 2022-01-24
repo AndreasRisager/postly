017022cb516de53cf4a4b57d9f29b9a6b655137a
Andreas <andreasbarkov@gmail.com>
2022-01-25 00:06:51 +0100

    add shopping page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Cart from "./views/Cart";
 import Checkout from "./views/Checkout";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import Shop from "./views/Shop";
 
 const stripePromise = loadStripe("pk_test_51KJOszIj4eKOJ7cWA4DXFTUzgHagumD73V2wyBTC4T6uKTAPOh3jTto0odReDAEwjukRVX1LEuqarK32Zb8XrZrT001Y1to1N7");
 
@@ -15,6 +16,7 @@ export default function App() {
         <Home path="/" default />
         <Cart path="/cart" />
         <Checkout path="/checkout" />
+        <Shop path="/shop" />
       </Router>
     </Elements>
   );
