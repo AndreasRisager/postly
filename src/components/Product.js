433f3886f86e23db43c36e50731460549c6b2d88
Andreas B. Risager <andreasbarkov@gmail.com>
2022-02-03 05:14:17 +0100

    remove link

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,16 +21,14 @@ export default function Product({ product, listView }) {
 
   return (
     <article className="product">
-      <Link to={`/product/${product.slug}`}>
-        <img src={product.image.url} alt={product.title} className="product__image" />
-        <div className="product__info">
-          <p className="product__title">{product.title}</p>
-          <p className="product__price">{product.price.toFixed(2)}&nbsp;kr</p>
-          <Link to={`/product/${product.slug}`} className="product__button">
-            vis produkt
-          </Link>
-        </div>
-      </Link>
+      <img src={product.image.url} alt={product.title} className="product__image" />
+      <div className="product__info">
+        <p className="product__title">{product.title}</p>
+        <p className="product__price">{product.price.toFixed(2)}&nbsp;kr</p>
+        <Link to={`/product/${product.slug}`} className="product__button">
+          vis produkt
+        </Link>
+      </div>
     </article>
   );
 }
